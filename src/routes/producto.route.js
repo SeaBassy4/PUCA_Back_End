81550fb00e693a711c56aa2eff2551677c0c5d4a
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-09-24 12:57:47 -0700

    chore: producto router finalizado

--- src/routes/producto.route.js
+++ src/routes/producto.route.js
@@ -8,6 +8,8 @@ const productoController =  require("../controllers/Producto.controller");
 //ROUTES
 
 router.get("/", productoController.getProductos);
-
+router.post("/", productoController.postProducto);
+router.put("/:id", productoController.putProducto);
+router.delete("/:id", productoController.deleteProducto);
 
 module.exports = router;
